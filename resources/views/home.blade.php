828fb8103edd76af40eadf7091bc7f02aae1683e
madeaditya02 <imadeaditya4@gmail.com>
2026-03-01 15:42:54 +0800

    Kode pertemuan 1

---
+++ resources/views/home.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Belgrano&family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap" rel="stylesheet">
+  @vite(['resources/css/style.css'])
+  <title>Document</title>
+</head>
+<body>
+  <header>
+    <h2 class="logo">Perspectra</h2>
+    <nav>
+      <a href="" class="nav-link">Home</a>
+      <a href="" class="nav-link">Stories</a>
+      <a href="" class="nav-link">Write</a>
+    </nav>
+    <div class="btn-group">
+      <a href="" class="btn-main">Sign In</a>
+      <a href="" class="btn-outline">Sign Up</a>
+    </div>
+  </header>
+
+  <div class="hero">
+    <section class="hero-content">
+      <h1 class="tagline">Stories That Shape How We See</h1>
+      <p class="deskripsi">A collection of thoughtful stories and perspectives that help you understand the world with greater clarity, empathy, and meaning</p>
+      <div class="hero-action">
+        <a href="" class="btn-main">Start Exploring</a>
+        <a href="" class="btn-outline">Share Your Story</a>
+      </div>
+    </section>
+    <img class="hero-img" src="/assets/hero.png" alt="">
+  </div>
+</body>
+</html>
