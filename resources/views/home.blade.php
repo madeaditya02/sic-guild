66ca21ed72e6fbbe8ed76611cad7a22a17c6f326
madeaditya02 <imadeaditya4@gmail.com>
2026-03-04 20:10:47 +0800

    Delete unnecesary js file

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,7 @@
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Belgrano&family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap" rel="stylesheet">
-  @vite(['resources/css/app.css', 'resources/js/app.js'])
+  @vite(['resources/css/app.css'])
   <title>Home - Perspectra</title>
 </head>
 <body>
